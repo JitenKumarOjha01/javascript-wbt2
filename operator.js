1da582ba38e45e1c55ed399ccacd6013afca1304
JitenKumarOjha01 <jitenkumarojha01@gmail.com>
2024-11-04 05:42:06 +0000

    operators

---
+++ operator.js
@@ -0,0 +1,25 @@
+//arithmatic operators-
+
+let a = 6;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+//comparison operator-
+
+
+console.log( 2 == 2) //equal to - true
+console.log( 3 != 3) //not equal to - 
+
+
+
+let x=3;
+
+//logical AND (&&)
+
+console.log(())
